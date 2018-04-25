e00a1bea47896506d8e4ad87066595ebc6671a6a
Mark <mak28530@users.noreply.github.com>
2018-04-25 16:58:44 -0700

    Connect Node.js backend to existing frontend UI

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 "use strict";
 
-var WINDOW_START = new Date(Date.now() + 0); // current time
-WINDOW_START.setHours(WINDOW_START.getHours() + 1); // Set to the next hour
-WINDOW_START.setMinutes(0); // Set the minutes to 0
-WINDOW_START.setSeconds(0); // Set the seconds to 0
-var WINDOW_END = new Date(WINDOW_START.valueOf() + (60 * 60 * 1000 * 72)); // 72 hours from now
-var WINDOW_INCREMENT = 60 * 60 * 1000; // 1 hour in milliseconds
+var API_ENDPOINT = 'https://comp680team4.herokuapp.com/api/';
+
+// If running locally, use the local API endpoint
+if (window.location.href === 'http://localhost:8888/') {
+  API_ENDPOINT = 'http://localhost:8888/api/';
+}
+// var WINDOW_START = new Date(Date.now() + 0); // current time
+// WINDOW_START.setHours(WINDOW_START.getHours() + 1); // Set to the next hour
+// WINDOW_START.setMinutes(0); // Set the minutes to 0
+// WINDOW_START.setSeconds(0); // Set the seconds to 0
+// WINDOW_START.setMilliseconds(0);  // Set the milliseconds to 0
+// var WINDOW_END = new Date(WINDOW_START.valueOf() + (60 * 60 * 1000 * 72)); // 72 hours from now
+// var WINDOW_INCREMENT = 60 * 60 * 1000; // 1 hour in milliseconds
 var DATETIME_FORMAT = 'dddd, MMMM Do [at] h:mm a';
-var HOUR_START = 6; // earliest time to start trip
-var HOUR_END = 20; // latest time to start trip
+// var HOUR_START = 6; // earliest time to start trip
+// var HOUR_END = 20; // latest time to start trip
 
 /**
  * Get the duration (with traffic) from a DirectionsResult object
@@ -70,6 +77,17 @@ function getEstimatedTravelTime(directionsService, startLocation, endLocation, d
   });
 }
 
+function showDebugTable(results) {
+  console.log('Departure time\t\t\t\t\t\t\tDuration');
+  var table = '<table id="debugTable" class="table table-sm table-bordered table-striped table-hover"><thead class="thead-dark"><tr><th scope="col">Departure time</th><th scope="col">Duration</th></tr></thead><tbody>';
+  results.forEach(function(result) {
+    console.log(moment(result.dateTime).format(DATETIME_FORMAT) + '\t\t\t\t' + result.durationText);
+    table += '<tr><td>' + moment(result.dateTime).format('L - LT') + '</td><td>' + result.durationText + '</td></tr>';
+  });
+  table += '</tbody></table>';
+  $('#output').after(table);
+}
+
 /**
  * Run the algorithm to determine the time of day / day of week that will give
  * you the shortest estimated travel time with traffic for a given trip
@@ -87,51 +105,41 @@ function runAlgorithm(directionsService, directionsDisplay) {
   console.log("Start location:\t" + startLocation);
   console.log("End location:\t" + endLocation);
 
-  var promiseThrottle = new PromiseThrottle({
-    requestsPerSecond: 1,           // up to 1 request per second
-    promiseImplementation: Promise  // the Promise library you are using
-  });
+  $.ajax({
+     url: API_ENDPOINT + "getShortestTravelTime",
+     method: "GET",
+     data: {
+       "startLocation": startLocation,
+       "endLocation": endLocation,
+     }
+   })
+   .done(function(results) {
+     console.log('Results');
+     console.log(results);
 
-  var currentDateTime = WINDOW_START;
-  var results = [];
+     console.log('Finding the shortest duration');
+     var bestTravelTime = _.min(results, 'duration');
+     console.log(bestTravelTime);
 
-  var sequence = [];
+     // Note: Can't use directionsDisplay.setDirections(bestTravelTime.response)
+     // since the Google Maps Web API response is not compatible with DirectionsDisplay
+     getEstimatedTravelTime(directionsService, startLocation, endLocation, new Date(bestTravelTime.dateTime))
+     .then(function(output) {
+       console.log(output);
+       directionsDisplay.setDirections(output.response);
 
-  while (currentDateTime <= WINDOW_END) {
-    if (currentDateTime.getHours() >= HOUR_START && currentDateTime.getHours() <= HOUR_END) {
-      sequence.push(promiseThrottle.add(getEstimatedTravelTime.bind(this, directionsService, startLocation, endLocation, currentDateTime)));
-    }
-    currentDateTime = new Date(currentDateTime.getTime() + WINDOW_INCREMENT);
-  }
+       showDebugTable(results);
 
-  // Promise that resolves when all requests have completed
-  Promise.all(sequence).then(function(results) {
-    console.log('Results');
-    console.log(results);
-
-    console.log('Departure time\t\t\t\t\t\t\tDuration');
-    var table = '<table id="debugTable" class="table table-sm table-bordered table-striped table-hover"><thead class="thead-dark"><tr><th scope="col">Departure time</th><th scope="col">Duration</th></tr></thead><tbody>';
-    results.forEach(function(result) {
-      console.log(moment(result.dateTime).format(DATETIME_FORMAT) + '\t\t\t\t' + result.durationText);
-      table += '<tr><td>' + moment(result.dateTime).format('L - LT') + '</td><td>' + result.durationText + '</td></tr>';
-    });
-    table += '</tbody></table>';
-    $('#output').after(table);
-
-
-    console.log('Finding the shortest duration');
-    var bestTravelTime = _.min(results, 'duration');
-    console.log(bestTravelTime);
-
-    directionsDisplay.setDirections(bestTravelTime.response);
-    $('#output').html('<div class="alert alert-success">The best time to leave is <strong>' + moment(bestTravelTime.dateTime).format(DATETIME_FORMAT) + '</strong>. Your travel time will be ' + bestTravelTime.durationText + ' with traffic.</div>');
-
-  }).catch(function(err) {
-    // catch any error that happened along the way
-    console.error("ERROR: " + err.message);
-    console.error(err);
-    $('#output').html('<div class="alert alert-danger">Uh oh: ' + err.message + '</div>');
-  })
+       // directionsDisplay.setDirections(bestTravelTime.response);
+       $('#output').html('<div class="alert alert-success">The best time to leave is <strong>' + moment(bestTravelTime.dateTime).format(DATETIME_FORMAT) + '</strong>. Your travel time will be ' + bestTravelTime.durationText + ' with traffic.</div>');
+     });
+   })
+   .fail(function(jqXHR, textStatus, o) {
+     // catch any error that happened along the way
+     console.error("ERROR: " + textStatus);
+     console.log(jqXHR);
+     $('#output').html('<div class="alert alert-danger">Uh oh: ' + textStatus + '</div>');
+   });
 }
 
 var map;
